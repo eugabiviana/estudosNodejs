6d0ef2f17442d90e1f516bbc5b30e1b6fc0ceca7
eugabiviana <kleber@gmail.com>
2021-08-24 10:17:59 -0300

    Praticando

---
+++ rocketseatDiscover/praticando/index.js
@@ -0,0 +1,9 @@
+//Praticando: variáveis e tipos de dados
+// declaração/declaration
+var name
+
+//assignment/ atribuição de valores
+name = "Gabi"
+
+//que tipo de dado foi colocado
+console.log(typeof name)
